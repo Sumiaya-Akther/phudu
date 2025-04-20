b71d0797e374052237a7a4c494545b9879185fc6
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-04-21 00:09:07 +0600

    first commit

---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+import logo from '../../assets/logo.png';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className='w-11/12 mx-auto flex justify-between'>
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow space-y-2">
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className={({ isActive }) =>
+                                        isActive ? "text-cyan-900 font-bold border-1 border-cyan-900 p-2 rounded" : ""
+                                    }
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/listedBooks"
+                                    className={({ isActive }) =>
+                                        isActive ? "text-cyan-900 font-bold border-1 border-cyan-900 p-2 rounded" : ""
+                                    }
+                                >
+                                    My-Bookings
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/pageRead"
+                                    className={({ isActive }) =>
+                                        isActive ? "text-cyan-900 font-bold border-1 border-cyan-900 p-2 rounded" : ""
+                                    }
+                                >
+                                    Blogs
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/pageRead"
+                                    className={({ isActive }) =>
+                                        isActive ? "text-cyan-900 font-bold border-1 border-cyan-900 p-2 rounded" : ""
+                                    }
+                                >
+                                    Contact Us
+                                </NavLink>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <h1 className="btn btn-ghost text-xl flex items-center"><img className='w-8' src={logo} alt="" />  Phudu</h1>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 text-gray-600 space-x-5">
+                        <li>
+                            <NavLink
+                                to="/"
+                                className={({ isActive }) =>
+                                    isActive ? "text-cyan-900 font-bold border-b-2 border-cyan-900 p-2" : ""
+                                }
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/listedBooks"
+                                className={({ isActive }) =>
+                                    isActive ? "text-cyan-900 font-bold border-b-2 border-cyan-900 p-2" : ""
+                                }
+                            >
+                                My-Bookings
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/pageRead"
+                                className={({ isActive }) =>
+                                    isActive ? "text-cyan-900 font-bold border-b-2 border-cyan-900 p-2" : ""
+                                }
+                            >
+                                Blogs
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/pageRead"
+                                className={({ isActive }) =>
+                                    isActive ? "text-cyan-900 font-bold border-b-2 border-cyan-900 p-2" : ""
+                                }
+                            >
+                                Contact Us
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn  btn-primary rounded-4xl">Emergency</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/pages/Root/Root.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../../components/Header/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../../components/Footer/Footer';
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className='w-11/12 mx-auto min-h-[calc(100vh-435px)] p-5 my-6'>
+            <Outlet></Outlet>
+            </div>
+            
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
